7e67a5b4ed53924cb04a712ed1789ff70b11322e
yayaya <carbonmonoxide0530@gmail.com>
2026-05-12 11:06:42 +0900

    Add files via upload

--- profile.js
+++ profile.js
@@ -65,7 +65,7 @@ function compressIcon(file) {
       const img = new Image();
 
       img.onload = () => {
-        const size = 120;
+        const size = 80;
 
         const canvas =
           document.createElement("canvas");
@@ -98,7 +98,7 @@ function compressIcon(file) {
         );
 
         const result =
-          canvas.toDataURL("image/jpeg", 0.28);
+          canvas.toDataURL("image/jpeg", 0.18);
 
         resolve(result);
       };
